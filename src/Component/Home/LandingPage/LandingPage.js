37ad9c51c8a13b03fc12672648c1230be5207ec1
Tanmay Jana <tanmay.jana@mountblue.io>
2019-08-29 22:58:58 +0530

    my first commit

---
+++ src/Component/Home/LandingPage/LandingPage.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import "./LandingPage.css";
+import GooglePlayLogo from "../../../assets/google-play-badge.png";
+import TryHardLogo from "../../../assets/429e9bf8b8a94aa87be06f9a038c4d1f014a0941.png";
+
+export default class LandingPage extends Component {
+  render() {
+    return (
+      <div className="landing-page-container">
+        <header>
+          <div className="logo-container">
+            <img
+              className="tryhard-logo"
+              src={TryHardLogo}
+              alt="tryhard-logo"
+            />
+            <p>Tryhard GG</p>
+          </div>
+          <div className = "nav-container">
+            <p>Features</p>
+            <p>About</p>
+            <button className = "login-button">LOG IN</button>
+          </div>
+        </header>
+        <div className="landing-body">
+          <h1>Welcome to the worlds first Esports rewards platform</h1>
+          <img
+            className="google-play-logo"
+            src={GooglePlayLogo}
+            alt="Google_play_logo"
+          />
+        </div>
+      </div>
+    );
+  }
+}
